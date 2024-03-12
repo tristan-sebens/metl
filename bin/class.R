533ccdbb6a13bd5c50d864c71a3a3b947d6c1a45
Tristan Sebens <tristan.sebens@noaa.gov>
2024-03-12 13:38:11 -0800

    Reorganized class definition code
    Consolidated all custom object definitions into a single 'class.R' file.
    
    Configurations are still kept separately

--- bin/class.R
+++ bin/class.R
@@ -1,4 +1,146 @@
-source(here::here('bin', 'field.R'))
+
+Field =
+  #' Field class. Represents a single data field and its key metadata for future
+  #`  reference, including field name, units, and DB data type.
+  #'
+  #' @field name character. The name of the field
+  #' @field units character. The SI units of the field
+  #' @field data_type character. The data type of this field in a DB
+  setRefClass(
+    "Field",
+    fields =
+      list(
+        name = "character",
+        id_field = "logical", # Flag to indicate if this is a field used to identify unique records
+        units = "character",
+        data_type = "character", # Data type to be used for this field in the DB
+        invert = "logical" # Flag to indicate if the values of the field should be inverted (x*-1)
+      ),
+    methods =
+      list(
+        initialize =
+          function(
+    ...,
+    invert = F
+          ) {
+            callSuper(
+              invert = invert,
+              ...
+            )
+          }
+      )
+  )
+
+
+FieldMap =
+  #' FieldMap class. Collection of Field objects, with some added functionality
+  #' to facilitate the convenient use of said Field objects both within the package
+  #' and when interfacing with a DB
+  #'
+  #' @field fields Field. The Field objects contained within this map/
+  setRefClass(
+    "FieldMap",
+    fields =
+      list(
+        # The DB table this FieldMap represents
+        table = "character",
+        # The list of fields
+        field_list = "list"
+      ),
+    methods =
+      list(
+        #' Generate a field data type list, in the format expected by the DBI package
+        #'
+        #' @return A named list. Names are the field names, values are the data types
+        generate_data_type_list =
+          function() {
+            .self$field_list %>%
+              reduce(
+                .init =
+                  list(),
+                .f =
+                  function(l, f) {
+                    l[f$name] = f$data_type
+                    return(l)
+                  }
+              )
+          },
+
+        #' Helper function which generates a list of the Fields shared by this
+        #' FieldMap and the given FieldMap
+        #'
+        #' @return A named list. Names are the field names, values are the data types
+        common_fields =
+          function(fm) {
+            .self$field_list[names(.self$field_list) %in% names(fm$field_list)]
+          },
+
+        # Generates a list of the Fields held in this FieldMap which are not in the passed FieldMap
+        uncommon_fields =
+          function(fm) {
+            .self$field_list[!names(.self$field_list) %in% names(fm$field_list)]
+          },
+
+        # Return the subset of Fields in this FieldMap that are marked as ID fields
+        get_id_fields =
+          function() {
+            Filter(
+              function(f) {f$id_field},
+              .self$field_list
+            )
+          },
+
+        # Get the names of the ID fields in this FieldMap
+        get_id_field_names =
+          function() {
+            .self$get_id_fields() %>%
+              lapply(
+                function(f) {
+                  f$name
+                }
+              ) %>%
+              unlist(use.names = F)
+          }
+      )
+  )
+
+#' Identifier class. Used to identify if a given directory belongs to a particular make/model of tag, based on available metadata
+#'
+#' Base class. Not intended to be implemented directly.
+Identifier =
+  setRefClass(
+    "Identifier",
+    methods =
+      list(
+        #' Returns TRUE/FALSE indicating if the metadata in the passed directory matches the patterns expected of the tag type with which this Identifier child class is associated
+        identify =
+          function(d) {
+            stop("Inheritence error: Invocation of identify function on base class Identifier. Please implement child class instead.")
+          },
+
+        #' Check number of files in directory which match pattern
+        #'
+        #' Helper function to quickly determine if an expected number of files
+        #' match the given pattern
+        #'
+        check_for_files =
+          function(d, pattern, n=1, ignore.case = T) {
+            # Check if the number of files which match the given pattern match the expected number
+            if(
+              length(
+                list.files(
+                  d,
+                  pattern = pattern,
+                  ignore.case = ignore.case
+                )
+              ) == n
+            ) {
+              return(TRUE)
+            }
+            return(FALSE)
+        }
+      )
+  )
 
 #' DataMap base class
 #'
@@ -414,8 +556,6 @@ Decoder =
       )
   )
   #----
-
-
 
 
 
