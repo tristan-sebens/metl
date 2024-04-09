60f53410b16c4990e25e9c30c2c5d92918d8ced8
Tristan Sebens <tristan.sebens@noaa.gov>
2024-04-09 13:42:09 -0800

    Update to DataMap_Base tests
    Updated tests and snaps of DataMap base to use new build_test_dataset and build_test_fieldmaps function

--- tests/testthat/test-DataMap_Base.R
+++ tests/testthat/test-DataMap_Base.R
@@ -2,8 +2,7 @@
 test_that(
   "DataMap::get_field_data",
   {
-    fm1 = build_test_fieldmaps()[[1]]
-    fm2 = build_test_fieldmaps()[[2]]
+    fm1 = build_test_fieldmaps()$INSTANT_DATA_INPUT_FIELD_MAP
 
     dm =
       DataMap()
@@ -11,7 +10,21 @@ test_that(
     expect_snapshot(
       dm$get_field_data(
         dat__ = build_test_dataset(),
-        input_field_obj_ = fm2$field_list[[4]]
+        input_field_obj_ = fm1$field_list$TIMESTAMP_FIELD
+      )
+    )
+
+    expect_snapshot(
+      dm$get_field_data(
+        dat__ = build_test_dataset(),
+        input_field_obj_ = fm1$field_list$DATA_FIELD_1
+      )
+    )
+
+    expect_snapshot(
+      dm$get_field_data(
+        dat__ = build_test_dataset(),
+        input_field_obj_ = fm1$field_list$DATA_FIELD_2
       )
     )
   }
@@ -20,14 +33,12 @@ test_that(
 test_that(
   "DataMap::transform_fields",
   {
-    fm1 = build_test_fieldmaps()[[1]]
-    fm2 = build_test_fieldmaps()[[2]]
 
     dm =
       .self =
       DataMap(
-        output_data_field_map = fm1,
-        input_data_field_map = fm2
+        input_data_field_map = build_test_fieldmaps()$INSTANT_DATA_INPUT_FIELD_MAP,
+        output_data_field_map = build_test_fieldmaps()$INSTANT_DATA_OUTPUT_FIELD_MAP
       )
 
     dat__ =
@@ -40,14 +51,14 @@ test_that(
 test_that(
   "DataMap::upsert",
   {
-    fm1 = build_test_fieldmaps()[[1]]
-    fm2 = build_test_fieldmaps()[[2]]
+    fm1 = build_test_fieldmaps()$INSTANT_DATA_INPUT_FIELD_MAP
+    fm2 = build_test_fieldmaps()$INSTANT_DATA_OUTPUT_FIELD_MAP
 
     dm =
       .self =
       DataMap(
-        output_data_field_map = fm1,
-        input_data_field_map = fm2
+        input_data_field_map = fm1,
+        output_data_field_map = fm2
       )
 
     # Get some test data
@@ -55,11 +66,14 @@ test_that(
       build_test_dataset()
 
     dat_t__ =
-      dm$transform(dat__)
+      dm$transform(dat__) %>%
+      # Manually insert the tag_id, required by the DB structure.
+      # This is typically done by the Decoder
+      dplyr::mutate(tag_id = 1)
 
     # Instantiate a connection to a temporary DB
     dats = list()
-    dats[[fm1$table]] = utils::head(dat_t__, as.integer(nrow(dat__)/10))
+    dats[[fm2$table]] = utils::head(dat_t__, as.integer(nrow(dat__)/10))
 
     con =
       build_test_db(dats = dats)
@@ -68,14 +82,14 @@ test_that(
     # Wrapped in a DF call bc the location of the DB will change between test
     # cycles, so we want to strip out the DB identifiers. We only care about
     # the table contents anyways
-    expect_snapshot(data.frame(dplyr::tbl(con, fm1$table)))
+    expect_snapshot(data.frame(dplyr::tbl(con, fm2$table)))
 
     # Perform the upsert
     # Check that the result is as expected
     expect_snapshot(dm$upsert(con, dat_t__))
 
     # Check that the new state of the DB table is as expected
-    expect_snapshot(data.frame(dplyr::tbl(con, fm1$table)))
+    expect_snapshot(data.frame(dplyr::tbl(con, fm2$table)))
 
     DBI::dbDisconnect(con)
   }
