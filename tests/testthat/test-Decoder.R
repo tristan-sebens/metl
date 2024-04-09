9212602afdce39d22eb4d9fc6cb38c0d3065372f
Tristan Sebens <tristan.sebens@noaa.gov>
2024-04-09 14:31:34 -0800

    Decoder tests and snaps
    More tests and snaps for the Decoder class

--- tests/testthat/test-Decoder.R
+++ tests/testthat/test-Decoder.R
@@ -97,6 +97,9 @@ test_that(
         )
       )
     )
+
+    # Tear down the test DB
+    DBI::dbDisconnect(con)
   }
 )
 
@@ -108,7 +111,49 @@ test_that(
 
     dm = .self = dc$data_maps[[1]]
 
-    dc$decode_datamap(dm)
+    expect_snapshot(dc$decode_datamap(dm))
+  }
+)
+
+test_that(
+  "Decoder::decode_and_load_all_datamaps",
+  {
+    dc = build_test_decoder()
+
+    # Build emtpy DB
+    con = build_test_db()
+
+    # Check that uploads go as expected
+    expect_snapshot(dc$decode_and_load_all_datamaps(con))
+
+    # Check states of tables
+    # Metadata table
+    expect_snapshot(
+      data.frame(
+        dplyr::tbl(
+          con,
+          dc$metadata_map$output_data_field_map$table
+        )
+      )
+    )
+    # Instant sensor data
+    expect_snapshot(
+      data.frame(
+        dplyr::tbl(
+          con,
+          dc$data_maps[[1]]$output_data_field_map$table
+        )
+      )
+    )
+    # Summary sensor data
+    expect_snapshot(
+      data.frame(
+        dplyr::tbl(
+          con,
+          dc$data_maps[[2]]$output_data_field_map$table
+        )
+      )
+    )
   }
 )
 
