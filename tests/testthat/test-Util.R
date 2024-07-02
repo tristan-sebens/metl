c0bd02da082171dc01b15b132cdbb8a4b038aa9b
Tristan Sebens <tristan.sebens@noaa.gov>
2024-07-02 13:50:35 -0800

    Tests for Util.R

---
+++ tests/testthat/test-Util.R
@@ -0,0 +1,40 @@
+test_that(
+  "build & populate test DB",
+  {
+
+    tbl1 =
+      data.frame(
+        "LETTER" = c("a", "b", "c", "d"),
+        "NUMBER" = c(1, 2, 3, 4)
+      )
+
+    tbl2 =
+      data.frame(
+        "KEY" = c("quickly", "brown", "fox"),
+        "VALUE" = c("adverb", "adjective", "noun")
+      )
+
+    db_conn =
+      build_temp_db(
+        to = tempfile(fileext = ".db"),
+        dats =
+          list(
+            "TABLE1" = tbl1,
+            "TABLE2" = tbl2
+          )
+      )
+
+    # Check that the returned connection object is of the correct class
+    expect_contains(class(db_conn), "SQLiteConnection")
+
+    # Check that all data from both data.frames is present in the DB
+    expect_equal(
+      data.frame(dplyr::tbl(db_conn, "TABLE1")),
+      tbl1
+    )
+    expect_equal(
+      data.frame(dplyr::tbl(db_conn, "TABLE2")),
+      tbl2
+    )
+  }
+)
